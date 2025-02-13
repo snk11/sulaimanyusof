544398fef1f3e4b1e4cbd31ef145bcb42a441c3b
snk11 <sul.yusof@gmail.com>
2025-02-13 01:46:37 +0100

    add

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import M1 from "./components/M1.tsx";
+import Expertise from "./components/Expertise.tsx";
+import Navigation from "./components/Navigation.tsx";
+import Footer from "./components/Footer.tsx";
+
+
+function App() {
+  /*
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
+  */
+
+
+{/*<div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
+<div className = "main-container">
+    <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
+    <FadeIn transitionDuration={700}>
+        <Main/>
+        <Expertise/>
+    </FadeIn>
+    <Footer />
+</div>*/}
+const [mode, setMode] = useState<string>('dark');
+
+    const handleModeChange = () => {
+        if (mode === 'dark') {
+            setMode('light');
+        } else {
+            setMode('dark');
+        }
+    }
+
+
+
+return (
+<div>
+            <h1>fdgfgfgfg</h1>
+          <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
+        
+            <M1/>
+            <Expertise/>
+        
+        <Footer />
+
+</div>
+
+
+);
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+//import GitHubIcon from '@mui/icons-material/GitHub';
+//import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Footer.scss'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        {/*
+        <a href="https://github.com/snk11" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/sulaiman-yusof-b3b507152/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
